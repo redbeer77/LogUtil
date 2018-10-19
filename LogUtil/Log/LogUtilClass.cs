1b22ff88ce5ba6a221bed7eb6dc1c4c9b592fed2
RedBeer77 <acastro1985@gmail.com>
2018-10-19 18:18:07 +0200

    first commit

---
+++ LogUtil/Log/LogUtilClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtil.Log
+{
+    public class LogUtilClass
+    {
+        private string nombreLog;
+        private string rutaLog;
+        private string path;
+        private DateTime init = DateTime.Now;
+        private DateTime end = DateTime.Now;
+
+        ~LogUtilClass()
+        {
+            this.EndLog();
+        }
+
+        public LogUtilClass()
+        {
+            NameValueCollection config = ConfigurationManager.GetSection("Rutes") as NameValueCollection;
+            if (config != null)
+            {
+                rutaLog = config["Log"].ToString();
+            }
+            config = ConfigurationManager.GetSection("Names") as NameValueCollection;
+            if (config != null)
+            {
+                nombreLog = config["Log"].ToString();
+            }
+
+            path = rutaLog + nombreLog;
+
+            FileStream fs = File.OpenWrite(rutaLog + nombreLog);
+            fs.Close();
+            this.InitLog();
+        }
+
+        private void PrintMessage(string texto)
+        {
+            if (File.ReadAllText(path).Length > 0)
+            {
+                File.AppendAllText(path, Environment.NewLine + texto, Encoding.UTF8);
+            }
+            else
+            {
+                File.AppendAllText(path, texto, Encoding.UTF8);
+            }
+        }
+        private void InitLog()
+        {
+            this.PrintMessage("Init " + DateTime.Now.ToString());
+        }
+        private void EndLog()
+        {
+            this.WriteInLog("Total: " + (end - init).ToString());
+            this.PrintMessage("End " + DateTime.Now.ToString());
+        }
+
+        public void WriteInLog(string text)
+        {
+
+            PrintMessage(String.Format("\t{0}", text));
+        }
+
+    }
+}
--- LogUtil/Program.cs
+++ LogUtil/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogUtil.Log;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace LogUtil
     {
         static void Main(string[] args)
         {
+            LogUtilClass log = new LogUtilClass();
+
+            log.WriteInLog("Example Text");
         }
     }
 }
